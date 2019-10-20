8a02b393f0466222e19b8813b345921787c7bc01
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-10-20 23:12:06 +0200

    add new array methods

--- arrays.js
+++ arrays.js
@@ -66,8 +66,103 @@
     console.log('Po: ' + tab); //Po: [4,3,2,1]
 }
 
-// sort
+// sort, localCompare
+
 
 {
+    const tab = ['Marcin', 'Ania', 'Piotrek', 'Grześ'];
+    tab.sort();
 
+    console.log(tab); //wypisze się "Ania, Grześ, Marcin, Piotrek"
+}
+
+{
+    function compareNr(a, b) {
+        return a - b
+    }
+
+    const tab = [100, 320, 10, 25, 310, 1200, 400];
+
+    const tab3 = tab.sort(compareNr);
+    console.log(tab3); //[10, 25, 100, 310, 320, 400, 1200]
+}
+
+{
+    const tab = [
+        { name: 'Marcin', height: 183 },
+        { name: 'Ania', height: 173 },
+        { name: 'Agnieszka', height: 170 },
+    ]
+
+    const tab2 = tab.sort(function (a, b) {
+        return a.name.localeCompare(b.name);
+    })
+
+    console.log(tab2);
+}
+
+{
+    const mails = [
+        "marcin@wp.pl",
+        "marcin@gmail.pl",
+        "marcin@onet.pl",
+        "marcin@interia.pl"
+    ]
+
+    //sortuje po domenach
+    //z użyciem funkcji strzałkowej
+    mails.sort((a, b) => {
+        const aDomain = a.substr(a.indexOf('@'));
+        const bDomain = b.substr(b.indexOf('@'));
+        return aDomain.localeCompare(bDomain);
+    })
+
+    console.log(mails);
+}
+
+{
+    const users = [
+        {
+            name: "Marcin",
+            car: {
+                name: "Toyota",
+                age: 10
+            }
+        },
+        {
+            name: "Marcin",
+            car: {
+                name: "Fiat",
+                age: 15
+            }
+        },
+        {
+            name: "Monika",
+            car: {
+                name: "BMW",
+                age: 5
+            }
+        },
+    ]
+
+    //sortuje po wieku samochodu
+    users.sort((a, b) => {
+        return a.car.age - b.car.age
+    });
+
+    console.log(users);
+}
+
+// concat
+
+{
+    const anim1 = ["Pies", "Kot"];
+    const anim2 = ["Słoń", "Wieloryb"];
+    const anim3 = ["Chomik ninja", "Świnka morderca"];
+
+    const table = anim1.concat(anim2);
+    console.log(table); //wypisze ["Pies", "Kot", "Słoń", "Wieloryb"]
+
+    const newTableBig = anim1.concat(anim2, anim3);
+    console.log(newTableBig); //wypisze ["Pies", "Kot", "Słoń", "Wieloryb", "Chomik ninja"];
 }

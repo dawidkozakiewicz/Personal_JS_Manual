17199be84ce5f25bc22595f7216b7db66d65449b
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-10-21 23:28:32 +0200

    arrays almost done

--- arrays.js
+++ arrays.js
@@ -166,3 +166,35 @@
     const newTableBig = anim1.concat(anim2, anim3);
     console.log(newTableBig); //wypisze ["Pies", "Kot", "Słoń", "Wieloryb", "Chomik ninja"];
 }
+
+// spread
+
+{
+    const anim1 = ["Pies", "Kot"];
+    const anim2 = ["Słoń", "Wieloryb"];
+    const table = [...anim1, ...anim2];
+    console.log(table)
+}
+
+// slice
+
+{
+    const tab = ['Marcin', 'Ania', 'Agnieszka', 'Monika', 'Magda'];
+
+    const tab2 = tab.slice(0, 1);
+    console.log(tab2); //[Marcin]
+    console.log(tab); //[Marcin, Ania, Agnieszka, Monika, Magda]
+
+    const tab3 = tab.slice(2);
+    console.log(tab3); //[Agnieszka, Monika. Magda]
+
+    const tab4 = tab.slice(0, 5);
+    console.log(tab4); //[Marcin, Ania, Agnieszka, Monika, Magda]
+
+    const tab5 = tab.slice(-2); //od końca
+    console.log(tab5); //[Monika, Magda]
+
+    const tab6 = tab.slice(2, -1);
+    console.log(tab6); //[Agnieszka, Monika]
+}
+

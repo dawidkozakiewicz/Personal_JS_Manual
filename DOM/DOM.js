6a52fbea143d46bce7001d9c5163de71426f230e
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-10-23 23:36:44 +0200

    add DOM methods

---
+++ DOM/DOM.js
@@ -0,0 +1,5 @@
+getElementById() // - pobiera jeden element o danym id
+getElementsByTagName() // - pobiera elementy o danym znaczniku
+getElementsByClassName() // - pobiera elementy o danej klasie
+querySelector() // - pobiera pierwszy element pasujący do selektora css
+querySelectorAll() // - pobiera elementy pasujące do selektora css

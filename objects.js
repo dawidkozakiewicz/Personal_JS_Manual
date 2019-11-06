231292098b3916defd037e9dd85a7614a2575226
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-11-06 20:21:15 +0100

    add object methods

--- objects.js
+++ objects.js
@@ -57,14 +57,76 @@ console.log(car.color); //undefined
         }
     }
 
-    for (const key in car) {
+    for (const key in car) { // !!! for in stosować tylko dla obiektów, nie dla tablic!!!
         console.log(key); //brand, color, speed, print
     }
 
 
-    for (const key in car) {
+    for (const key in car) { // !!! for in stosować tylko dla obiektów, nie dla tablic!!!
         console.log("Klucz: ", key);
         console.log("Wartość: ", car[key]);
     }
 
+}
+
+// pobranie kluczy i ich wartości:
+
+{
+    const calendar = {
+        "2018-11-11": {
+            name: "Narodowe Święto Niepodległości"
+        },
+        "2018-12-24": {
+            name: "Wigilia Bożego Narodzenia"
+        }
+    }
+
+    for (const key in calendar) {
+        console.log("Święta na dzień: " + key);
+        console.log(calendar[key]);
+    }
+}
+
+// Object.keys(obj) - zwraca tablicę kluczy obiektu
+
+{
+    const car = {
+        brand: "Mercedes",
+        color: "czerwony",
+        speed: 150
+    }
+
+    const keys = Object.keys(car);
+    for (const key of keys) { //pętla po tablicy! - of, nie in
+        console.log(car[key]);
+    }
+}
+
+//tablica w której chcemy zliczyć występowanie danych elementów
+
+{
+    const tab = [
+        "Ala ma kota",
+        3,
+        "Ania lubi czekoladki",
+        "Ala ma kota",
+        2,
+        "Piesek Rysiek",
+        "Piesek Rysiek",
+        2,
+        "Ania lubi czekoladki",
+        "Ania lubi czekoladki"
+    ];
+
+
+    const ob = {};
+    tab.forEach(function (el) {
+        //jeżeli właściwości o takiej nazwie nasz obiekt jeszcze nie ma to ją tworzymy
+        if (!ob.hasOwnProperty(el)) {
+            ob[el] = 0;
+        }
+        ob[el]++;
+    })
+
+    console.log(ob);
 }

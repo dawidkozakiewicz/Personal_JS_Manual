5186a012b7f7b45318dfcb9f30e37f226ce3c7eb
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-12-07 23:04:19 +0100

    exploring prototypes

--- objects/Kontruktor-i-instancje/main.js
+++ objects/Kontruktor-i-instancje/main.js
@@ -1,23 +1,122 @@
-/* KONSTRUKTOR I INSTANCJA */
+/* PROTOTYP */
 
-// Konstruktor to funkcja. Konwencja nakazuje by nazwa konstruktura pisana była wielką literą.
-// konstruktor tworzymy za pomocą wyrażenie (przypisanie do zmiennej) lub deklracja. oba przykłądy ponizej.
-
-// const Car = function() { }
-
-function Car(name) {
- // console.log(this);
+/* CZĘŚĆ 1 */
+const Person = function(name, age) {
  this.name = name;
+ this.age = age;
+ this.children = [];
+ // this.addChildren = function(name) {
+ //  this.children.push(name);
+ // }
 }
 
-// Instanacja (egzemplarz) - obiekt stworzony w oparciu o wzór w konstruktorze. Posiada także odwołanie do prototypu constructora.
+// obiekt prototype znajduje się w konstruktorze i możemy do niego przypisywać metody do których dostęp mają wszystkie instancje (egzemplarze) stworzoene w oparciu o konstruktor
 
-const maluch = new Car('maluch');
-const polonez = new Car('polonez');
-// JAK DZIAŁA TWORZENIE NOWEGO EGZEMPLARZA (INSTANCJI)
-// 1. new + funkcja konstruktora - powstaje nowy  obiekt na którym wywoływana jest funkcja konstruktora.
-// 2. następuje wiązanie this z utworzonym obiektem
-// 3. W nowym obiekcie powstaje własciwość __proto__ wskazująca na prototyp konstruktora
-// 4. Przypisanie nowego obiektu do zmiennej
+Person.prototype.addChildren = function(name) {
+ this.children.push(name);
+}
 
-maluch.name = "zabytek"
+// Do prototypu zdecydowanie częsciej przypisujemy funkcje (a więc metody) niż inne typy.
+Person.prototype.gender = 'female';
+
+const arek = new Person('arek', 20);
+const monika = new Person('monika', 30);
+
+monika.addChildren('basia')
+
+/* CZĘŚĆ 2 */
+
+// CZYM JEST PROTOTYP
+
+// Prototyp to obiekt w funkcji konstruktora, współdzielony przez wszystkie egzemplarze. Kązdy egzemplarz ma dostęp do obiektu prototypu.
+
+// CO ZAWIERA PROTOTYP?
+// domyślnie właściwość constructor (funkcja konstruktora lub klasa) + to co zostanie przypisane do prototypu
+
+function Player() {
+}
+
+class User {
+
+}
+
+
+Player.prototype.age = 25;
+const janek = new Player();
+const marta = new User();
+
+janek.constructor
+marta.constructor
+
+const darek = new janek.constructor()
+
+
+// DODANIE ELEMENTÓW DO PROTOTYPU (KONSTRUKTOR)
+function Citizen(country, citizenship) {
+ this.country = country;
+ this.citizenship = citizenship;
+ this.changeCitizenship = function(citizenship) {
+  this.citizenship = citizenship;
+  console.log(`Zmiana za pomocą metody w instancji na obywatelstwo ${this.citizenship}`)
+ }
+}
+
+Citizen.prototype.changeCitizenship = function(citizenship) {
+ this.citizenship = citizenship;
+ console.log(`Zmiana za pomocą metody w protypie na obywatelstwo ${this.citizenship}`)
+}
+
+const zenek = new Citizen("Polska", "polskie");
+const marysia = new Citizen("Francja", "niemieckie");
+
+// zenek.changeCitizenship('rosyjskie')
+
+// Obiekt prototype zawsze aktualny
+// console.log(zenek.age);
+Citizen.prototype.age = 21;
+// console.log(zenek.age);
+
+// Przysłanianie
+
+// Rozszerzanie prototypu także wbudowanych konstruktorów
+const arr = [5, 6, 7, 8];
+Array.prototype.delete = function(index) {
+ return this.splice(index, 1)
+}
+
+
+// CZĘŚĆ 3
+
+// ŁAŃCUCH PROTOTYPÓW (PROTOTYPE CHAIN)
+
+arr.__proto__ //protyp konstruktora
+arr.__proto__.__proto__ //protyp Object
+arr.__proto__.__proto__.__proto__ //null
+//arr.age = 20
+// proto1.age = 20
+Object.prototype.age = 20;
+
+zenek.__proto__.__proto__.__proto__
+
+// KILKA INNYCH ISTOTNYCH ELEMENTÓW
+// Instanceof 
+
+arr instanceof Array;
+arr instanceof Object;
+arr instanceof Citizen;
+
+zenek instanceof Citizen;
+zenek instanceof Object;
+zenek instanceof Function;
+
+
+// Object.getPrototypeOf 
+Object.getPrototypeOf(arr)
+Object.getPrototypeOf(zenek)
+
+// constructor czy prototyp - co łączy instancję z funkcją konstruktora (także  klasą) 
+
+const janusz = new Citizen()
+//janusz.__proto__ = Citizen.prototype
+
+

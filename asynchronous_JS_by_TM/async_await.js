d4e48c79fb5ef63bb61bb3a07224526921f49276
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-03-02 23:02:09 +0100

    add async-await

---
+++ asynchronous_JS_by_TM/async_await.js
@@ -0,0 +1,47 @@
+const posts = [
+    { title: "Post One", body: "This is post one" },
+    { title: "Post Two", body: "This is post two" }
+  ];
+  
+  getPosts = () => {
+    setTimeout(() => {
+      let output = "";
+      posts.forEach((post, index) => {
+        output += `<li>${post.title}</li>`;
+      });
+      document.body.innerHTML = output;
+    }, 1000);
+  };
+  
+  createPost = post => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        posts.push(post);
+        if (typeof post === "object") {
+          resolve();
+        } else {
+          reject("Error: Something went wrong");
+        }
+      }, 2000);
+    });
+  };
+
+   init = async () => {
+      await createPost({ title: "Post Three", body: "This is post three" })
+      getPosts()
+  }
+
+  init()
+
+// *******************************************************************************************
+
+
+  fetchUsers = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+      console.log(res)
+      const data = await res.json()
+      console.log(data)
+  }
+
+  fetchUsers()
+
--- asynchronous_JS_by_TM/promise.js
+++ asynchronous_JS_by_TM/promise.js
@@ -29,3 +29,42 @@ createPost = post => {
 createPost({ title: "Post Three", body: "This is post three" })
   .then(getPosts)
   .catch(err => console.log(err));
+
+// *******************************************************************************************
+
+const promise1 = Promise.resolve("Hello World");
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+  setTimeout(resolve, 2000, "Goodbye")
+);
+const promise4 = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
+  res.json()
+);
+
+Promise.all([promise1, promise2, promise3, promise4]).then(values =>
+  console.log(values)
+);
+
+// *******************************************************************************************
+
+const loadData = function(name) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", () => resolve(xhr.response));
+    xhr.addEventListener("error", () => reject(xhr.statusText));
+    xhr.open(
+      "GET",
+      `https://restcountries.eu/rest/v2/name/${name}?fullText=true`,
+      true
+    );
+    xhr.send();
+  });
+};
+
+loadData("poland")
+  .then(response => {
+    console.log(response);
+  })
+  .catch(statusText => {
+    console.log(statusText);
+  });

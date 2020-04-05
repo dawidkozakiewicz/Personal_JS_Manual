b28c24123a7791aced3bb5fc04cde19c7a795e84
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-04-05 23:36:27 +0200

    import all

--- ES6_po_polsku/modules/index2.js
+++ ES6_po_polsku/modules/index2.js
@@ -1,6 +1,10 @@
 import {uppercaseString, lowercaseString} from './index.js'
+import * as someFunctions from './index3.js'
 
 
 console.log(uppercaseString("hello"));
 
 console.log('sdjkfhsdljkfhl')
+
+someFunctions.func1();
+someFunctions.func2();
---
+++ ES6_po_polsku/modules/index3.js
@@ -0,0 +1,9 @@
+const func1 = () => {
+    console.log('Im from index3.js file')
+}
+
+const func2 = () => {
+    console.log('Im from index3.js file too')
+}
+
+export {func1, func2}

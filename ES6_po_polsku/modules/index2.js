57c566d071ba4b976883c321502b32548304863c
Dawid Kozakiewicz <turgor1996@gmail.com>
2020-04-06 23:15:09 +0200

    export default

--- ES6_po_polsku/modules/index2.js
+++ ES6_po_polsku/modules/index2.js
@@ -1,9 +1,10 @@
 import {uppercaseString, lowercaseString} from './index.js'
 import * as someFunctions from './index3.js'
+import add from "./default.js";
 
 
 console.log(uppercaseString("hello"));
-
+console.log(add(3,6))
 console.log('sdjkfhsdljkfhl')
 
 someFunctions.func1();
